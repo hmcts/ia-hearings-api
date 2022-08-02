ac6ef2a39db51812d38760829104d7f135e0a0f2
Dbarnes-v1 <daniel.barnes@hmcts.net>
2022-08-02 17:17:07 +0100

    RIA-5975 - Checkstyle fix & feign retry added

--- src/main/java/uk/gov/hmcts/reform/iahearingsapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/iahearingsapi/Application.java
@@ -1,10 +1,28 @@
 package uk.gov.hmcts.reform.iahearingsapi;
 
+import com.microsoft.applicationinsights.web.internal.ApplicationInsightsServletContextListener;
+import java.util.concurrent.TimeUnit;
+import javax.servlet.ServletContextListener;
+import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.reform.iahearingsapi.domain.entities.ccd.CcdRequestDetails;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableRetry
+@ComponentScan(basePackages = {"uk.gov.hmcts.reform"})
 @EnableFeignClients(basePackages =
     {
         "uk.gov.hmcts.reform.auth",
@@ -16,5 +34,53 @@ public class Application {
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public OkHttpClient okHttpClient() {
+        int timeout = 10;
+        return new OkHttpClient.Builder()
+            .connectTimeout(timeout, TimeUnit.MINUTES)
+            .readTimeout(timeout, TimeUnit.MINUTES)
+            .retryOnConnectionFailure(true)
+            .build();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<ServletContextListener> appInsightsServletContextListenerRegistrationBean(
+        ApplicationInsightsServletContextListener applicationInsightsServletContextListener) {
+        ServletListenerRegistrationBean<ServletContextListener> srb =
+            new ServletListenerRegistrationBean<>();
+        srb.setListener(applicationInsightsServletContextListener);
+        return srb;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ApplicationInsightsServletContextListener applicationInsightsServletContextListener() {
+        return new ApplicationInsightsServletContextListener();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource bean = new ReloadableResourceBundleMessageSource();
+        bean.setBasename("classpath:application");
+        bean.setDefaultEncoding("UTF-8");
+        return bean;
+    }
+
+    @Bean
+    public CcdRequestDetails getRequestDetails(
+        @Value("${core_case_data.jurisdictionId}") String coreCaseDataJurisdictionId,
+        @Value("${core_case_data.caseTypeId}") String coreCaseDataCaseTypeId) {
+        return CcdRequestDetails.builder()
+            .caseTypeId(coreCaseDataCaseTypeId)
+            .jurisdictionId(coreCaseDataJurisdictionId)
+            .build();
     }
 }
--- src/main/java/uk/gov/hmcts/reform/iahearingsapi/domain/service/HmcHearingApi.java
+++ src/main/java/uk/gov/hmcts/reform/iahearingsapi/domain/service/HmcHearingApi.java
@@ -4,7 +4,9 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import uk.gov.hmcts.reform.iahearingsapi.domain.model.single.hearing.HearingRequestPayload;
 import uk.gov.hmcts.reform.iahearingsapi.domain.model.single.hearing.HmcUpdateResponse;
 import uk.gov.hmcts.reform.iahearingsapi.infrastructure.config.FeignConfiguration;
