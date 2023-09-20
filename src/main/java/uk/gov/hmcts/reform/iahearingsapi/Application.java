04815cbf8206332576279d73936b65aed368fdc8
Augustine Onubeze <augustine.onubeze@hmcts.net>
2023-09-20 17:08:18 +0100

    RIA-7867: Refactor the functionality to send a test hearing update message

--- src/main/java/uk/gov/hmcts/reform/iahearingsapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/iahearingsapi/Application.java
@@ -24,27 +24,36 @@ public class Application {
 
     public static void main(final String[] args) {
 
-        // SpringApplication.run(Application.class, args);
-
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 
-        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+        sendTestHearingUpdateMessage(context);
+    }
 
-        HmcMessage hmcMessage = HmcMessage.builder()
-            .caseId(123456789L)
-            .hearingId("hearingId")
-            .hmctsServiceCode("BFA1")
-            .hearingUpdate(HearingUpdate.builder()
-               .hearingJudgeId("hearingJudgeId")
-               .hearingRoomId("hearingRoomId")
-               .hmcStatus(HmcStatus.LISTED)
-               .hearingVenueId("hearingVenueId")
-               .hearingListingStatus("hearingListingStatus")
-               .listAssistCaseStatus("listAssistCaseStatus")
-               .listAssistSessionID("listAssistSessionID")
-               .build())
-            .build();
-        log.info("Sending a hearing update message.");
-        jmsTemplate.convertAndSend("ia-hmc-topic", hmcMessage);
+    public static void sendTestHearingUpdateMessage(ConfigurableApplicationContext context) {
+
+        boolean enabled = Boolean.TRUE.equals(context.getEnvironment().getProperty(
+            "flags.test-hearings-update-message.enabled",
+            Boolean.class
+        ));
+        if (enabled) {
+            JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+
+            HmcMessage hmcMessage = HmcMessage.builder()
+                .caseId(1234567891234L)
+                .hearingId("hearingId")
+                .hmctsServiceCode("BFA1")
+                .hearingUpdate(HearingUpdate.builder()
+                                   .hearingJudgeId("hearingJudgeId")
+                                   .hearingRoomId("hearingRoomId")
+                                   .hmcStatus(HmcStatus.LISTED)
+                                   .hearingVenueId("hearingVenueId")
+                                   .hearingListingStatus("hearingListingStatus")
+                                   .listAssistCaseStatus("listAssistCaseStatus")
+                                   .listAssistSessionID("listAssistSessionID")
+                                   .build())
+                .build();
+            log.info("Sending a hearing update message.");
+            jmsTemplate.convertAndSend("ia-hmc-topic", hmcMessage);
+        }
     }
 }
