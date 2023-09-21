20d09fda93de2712aff9ffe922f301e9b67b6dc3
Augustine Onubeze <augustine.onubeze@hmcts.net>
2023-09-21 09:12:20 +0100

    RIA-7867: Add more unit tests to pass test coverage

--- src/test/java/uk/gov/hmcts/reform/iahearingsapi/domain/service/HearingServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/iahearingsapi/domain/service/HearingServiceTest.java
@@ -3,11 +3,14 @@ package uk.gov.hmcts.reform.iahearingsapi.domain.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import feign.FeignException;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,6 +26,8 @@ import uk.gov.hmcts.reform.iahearingsapi.domain.entities.AsylumCase;
 import uk.gov.hmcts.reform.iahearingsapi.domain.entities.HearingRequestPayload;
 import uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.HearingGetResponse;
 import uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.ServiceHearingValuesModel;
+import uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.response.PartiesNotified;
+import uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.response.PartiesNotifiedResponses;
 import uk.gov.hmcts.reform.iahearingsapi.infrastructure.clients.HearingRequestGenerator;
 import uk.gov.hmcts.reform.iahearingsapi.infrastructure.clients.HmcHearingApi;
 import uk.gov.hmcts.reform.iahearingsapi.infrastructure.clients.model.hmc.HmcHearingRequestPayload;
@@ -55,11 +60,17 @@ class HearingServiceTest {
     @InjectMocks
     private HearingService hearingService;
 
+    private LocalDateTime receivedDateTime;
+    private PartiesNotified payload;
+
     @BeforeEach
     void setup() {
         when(idamService.getServiceUserToken()).thenReturn(IDAM_OAUTH2_TOKEN);
         when(serviceAuthTokenGenerator.generate()).thenReturn(SERVICE_AUTHORIZATION);
         when(coreCaseDataService.getCase(CASE_ID)).thenReturn(asylumCase);
+
+        receivedDateTime = LocalDateTime.now();
+        payload = PartiesNotified.builder().build();
     }
 
     @Test
@@ -130,5 +141,61 @@ class HearingServiceTest {
         List<Object> result = hearingService.getHearingLinkData(new HearingRequestPayload(CASE_ID));
 
         assertThat(result).isNotNull();
+    }
+
+    @Test
+    void testGetPartiesNotified() {
+        when(hmcHearingApi.getPartiesNotifiedRequest(IDAM_OAUTH2_TOKEN, SERVICE_AUTHORIZATION, HEARING_ID))
+            .thenReturn(new PartiesNotifiedResponses());
+
+        PartiesNotifiedResponses result = hearingService.getPartiesNotified(HEARING_ID);
+
+        assertThat(result).isNotNull();
+    }
+
+    @Test
+    void testGetPartiesNotifiedException() {
+        when(idamService.getServiceUserToken()).thenReturn("serviceUserToken");
+        when(serviceAuthTokenGenerator.generate()).thenReturn("serviceAuthToken");
+        when(hmcHearingApi.getPartiesNotifiedRequest(anyString(), anyString(), anyString()))
+            .thenThrow(FeignException.class);
+
+        assertThrows(HmcException.class, () -> {
+            hearingService.getPartiesNotified(HEARING_ID);
+        });
+    }
+
+    @Test
+    void testUpdatePartiesNotified() {
+
+        hearingService.updatePartiesNotified(HEARING_ID, 1, receivedDateTime, payload);
+
+        verify(hmcHearingApi, times(1)).updatePartiesNotifiedRequest(
+            IDAM_OAUTH2_TOKEN,
+            SERVICE_AUTHORIZATION,
+            payload,
+            HEARING_ID,
+            1,
+            receivedDateTime);
+    }
+
+    @Test
+    void testUpdatePartiesNotifiedException() {
+
+        when(idamService.getServiceUserToken()).thenReturn("serviceUserToken");
+        when(serviceAuthTokenGenerator.generate()).thenReturn("serviceAuthToken");
+        doThrow(FeignException.class)
+            .when(hmcHearingApi)
+            .updatePartiesNotifiedRequest(
+                anyString(),
+                anyString(),
+                any(PartiesNotified.class),
+                anyString(),
+                anyInt(),
+                any(LocalDateTime.class));
+
+        assertThrows(HmcException.class, () -> {
+            hearingService.updatePartiesNotified(HEARING_ID, 1, receivedDateTime, payload);
+        });
     }
 }
