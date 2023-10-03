770f44b5bab65d06460ecb014320e6697a49e8ca
robin-ieong <robin.ieong@version1.com>
2023-10-03 12:19:54 +0100

    RIA-7668: To display Respondent as an individual in the Hearings tab Participant attendance page

--- src/main/java/uk/gov/hmcts/reform/iahearingsapi/domain/mappers/RespondentDetailsMapper.java
+++ src/main/java/uk/gov/hmcts/reform/iahearingsapi/domain/mappers/RespondentDetailsMapper.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.iahearingsapi.domain.mappers;
 
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iahearingsapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.IndividualDetailsModel;
 import uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.OrganisationDetailsModel;
 import uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.PartyDetailsModel;
 import uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.PartyType;
@@ -13,8 +14,15 @@ public class RespondentDetailsMapper {
 
         return PartyDetailsModel.builder()
             .partyID(caseDataMapper.getRespondentPartyId(asylumCase))
-            .partyType(PartyType.ORG.getPartyType())
+            .partyType(PartyType.IND.getPartyType())
             .partyRole("RESP")
+            // For displaying Respondent as an individual in the Hearings tab Participant attendance page.
+            .individualDetails(
+                IndividualDetailsModel.builder()
+                    .firstName("Home")
+                    .lastName("Office")
+                    .preferredHearingChannel(caseDataMapper.getHearingChannel(asylumCase))
+                    .build())
             .organisationDetails(
                 OrganisationDetailsModel.builder()
                         .organisationType(PartyType.ORG.getPartyType())
