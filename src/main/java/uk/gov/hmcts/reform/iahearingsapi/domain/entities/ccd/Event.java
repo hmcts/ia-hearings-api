05cfd1f8e0e08ce41eabc2aa773fe0d10de0a9f4
cmccarthyHMCTS <138468823+cmccarthyHMCTS@users.noreply.github.com>
2023-10-16 16:07:01 +0100

    RIA-7819 trigger new update hmc event (#197)
    * RIA-7819: new event triggered on response from hmc
    
    * RIA-7819: new event triggered on response from hmc
    
    * RIA-7819: new event triggered on response from hmc
    
    * RIA-7819: updated unit tests
    
    * RIA-7819: updated unit tests
    
    * RIA-7819: removed unused code
    
    * RIA-7819: removed unused code
    
    ---------
    
    Co-authored-by: McCarthyC <cmccarthy95@icloud.com>

--- src/main/java/uk/gov/hmcts/reform/iahearingsapi/domain/entities/ccd/Event.java
+++ src/main/java/uk/gov/hmcts/reform/iahearingsapi/domain/entities/ccd/Event.java
@@ -7,7 +7,7 @@ public enum Event {
 
     LIST_CASE("listCase"),
     HANDLE_HEARING_EXCEPTION("handleHearingException"),
-
+    UPDATE_HMC_RESPONSE("updateHMCResponse"),
     @JsonEnumDefaultValue
     UNKNOWN("unknown");
 
