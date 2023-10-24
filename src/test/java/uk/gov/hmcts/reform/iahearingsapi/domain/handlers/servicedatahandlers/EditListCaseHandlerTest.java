93442ce6d646258c30200bc2a7d9dc11dd702268
Miguel Martin <miguel.martin1@hmcts.net>
2023-10-24 12:56:43 +0200

    RIA-7882 Fix EditListCaseHandlerTests

--- src/test/java/uk/gov/hmcts/reform/iahearingsapi/domain/handlers/servicedatahandlers/EditListCaseHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iahearingsapi/domain/handlers/servicedatahandlers/EditListCaseHandlerTest.java
@@ -39,7 +39,7 @@ import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.AsylumCaseFieldD
 import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.AsylumCaseFieldDefinition.LIST_CASE_HEARING_DATE;
 import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.AsylumCaseFieldDefinition.LIST_CASE_HEARING_LENGTH;
 import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.ServiceDataFieldDefinition.DURATION;
-import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.ccd.Event.LIST_CASE;
+import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.ccd.Event.EDIT_CASE_LISTING;
 import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.HearingType.COSTS;
 import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.HearingType.SUBSTANTIVE;
 
@@ -61,20 +61,20 @@ class EditListCaseHandlerTest {
     ServiceData serviceData;
     @Mock
     AsylumCase asylumCase;
-    private ListCaseHandler listCaseHandler;
+    private EditListCaseHandler editListCaseHandler;
 
     @BeforeEach
     public void setUp() {
 
-        listCaseHandler =
-            new ListCaseHandler(coreCaseDataService);
+        editListCaseHandler =
+            new EditListCaseHandler(coreCaseDataService);
 
         when(serviceData.read(ServiceDataFieldDefinition.HMC_STATUS, HmcStatus.class))
             .thenReturn(Optional.of(HmcStatus.LISTED));
         when(serviceData.read(ServiceDataFieldDefinition.HEARING_LISTING_STATUS, ListingStatus.class))
             .thenReturn(Optional.of(ListingStatus.FIXED));
         when(serviceData.read(ServiceDataFieldDefinition.LIST_ASSIST_CASE_STATUS, ListAssistCaseStatus.class))
-            .thenReturn(Optional.of(ListAssistCaseStatus.LISTED));
+            .thenReturn(Optional.of(ListAssistCaseStatus.PREPARE_FOR_HEARING));
         when(serviceData.read(ServiceDataFieldDefinition.HEARING_CHANNELS))
             .thenReturn(Optional.of(List.of(HearingChannel.INTER)));
         when(serviceData.read(ServiceDataFieldDefinition.HEARING_TYPE, String.class))
@@ -83,47 +83,47 @@ class EditListCaseHandlerTest {
 
     @Test
     void should_have_early_dispatch_priority() {
-        assertEquals(DispatchPriority.EARLY, listCaseHandler.getDispatchPriority());
+        assertEquals(DispatchPriority.EARLY, editListCaseHandler.getDispatchPriority());
     }
 
     @Test
     void should_handle_only_if_service_data_qualifies() {
-        assertTrue(listCaseHandler.canHandle(serviceData));
+        assertTrue(editListCaseHandler.canHandle(serviceData));
     }
 
     @Test
     void should_not_handle_if_hearing_type_unqualified() {
         when(serviceData.read(ServiceDataFieldDefinition.HEARING_TYPE, String.class))
             .thenReturn(Optional.of(COSTS.getKey()));
-        assertFalse(listCaseHandler.canHandle(serviceData));
+        assertFalse(editListCaseHandler.canHandle(serviceData));
     }
 
     @Test
     void should_not_handle_if_hmc_status_unqualified() {
         when(serviceData.read(ServiceDataFieldDefinition.HMC_STATUS, HmcStatus.class))
             .thenReturn(Optional.of(HmcStatus.CLOSED));
-        assertFalse(listCaseHandler.canHandle(serviceData));
+        assertFalse(editListCaseHandler.canHandle(serviceData));
     }
 
     @Test
     void should_not_handle_if_hearing_channels_on_papers() {
         when(serviceData.read(ServiceDataFieldDefinition.HEARING_CHANNELS, List.class))
             .thenReturn(Optional.of(List.of(HearingChannel.ONPPRS)));
-        assertFalse(listCaseHandler.canHandle(serviceData));
+        assertFalse(editListCaseHandler.canHandle(serviceData));
     }
 
     @Test
     void should_not_handle_if_hearing_listing_status_unqualified() {
         when(serviceData.read(ServiceDataFieldDefinition.HEARING_LISTING_STATUS, ListingStatus.class))
             .thenReturn(Optional.of(ListingStatus.DRAFT));
-        assertFalse(listCaseHandler.canHandle(serviceData));
+        assertFalse(editListCaseHandler.canHandle(serviceData));
     }
 
     @Test
     void should_not_handle_if_list_assist_case_status_unqualified() {
         when(serviceData.read(ServiceDataFieldDefinition.LIST_ASSIST_CASE_STATUS, ListAssistCaseStatus.class))
             .thenReturn(Optional.of(ListAssistCaseStatus.CASE_CLOSED));
-        assertFalse(listCaseHandler.canHandle(serviceData));
+        assertFalse(editListCaseHandler.canHandle(serviceData));
     }
 
     @Test
@@ -131,7 +131,7 @@ class EditListCaseHandlerTest {
         when(serviceData.read(ServiceDataFieldDefinition.LIST_ASSIST_CASE_STATUS, ListAssistCaseStatus.class))
             .thenReturn(Optional.of(ListAssistCaseStatus.CASE_CLOSED));
 
-        assertThrows(IllegalStateException.class, () -> listCaseHandler.handle(serviceData));
+        assertThrows(IllegalStateException.class, () -> editListCaseHandler.handle(serviceData));
     }
 
     @Test
@@ -141,14 +141,14 @@ class EditListCaseHandlerTest {
         when(serviceData.read(ServiceDataFieldDefinition.NEXT_HEARING_DATE, LocalDateTime.class))
             .thenReturn(Optional.of(NEXT_HEARING_DATE.plusDays(1)));
 
-        listCaseHandler.handle(serviceData);
+        editListCaseHandler.handle(serviceData);
 
         verify(asylumCase).write(
             LIST_CASE_HEARING_DATE,
             LocalDateTime.of(2023, 9, 30, 9, 45)
                 .format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS"))
         );
-        verify(coreCaseDataService).triggerEvent(LIST_CASE, CASE_REF, asylumCase);
+        verify(coreCaseDataService).triggerEvent(EDIT_CASE_LISTING, CASE_REF, asylumCase);
     }
 
     @Test
@@ -158,19 +158,14 @@ class EditListCaseHandlerTest {
         when(serviceData.read(ServiceDataFieldDefinition.HEARING_VENUE_ID, String.class))
             .thenReturn(Optional.of(HearingCentre.BRADFORD.getEpimsId()));
 
-        listCaseHandler.handle(serviceData);
+        editListCaseHandler.handle(serviceData);
 
         Map<String, Object> caseData = getCaseDataMapping();
         caseData.put(LIST_CASE_HEARING_CENTRE.value(), HearingCentre.BRADFORD);
         verify(asylumCase).write(
             LIST_CASE_HEARING_CENTRE, HearingCentre.BRADFORD
         );
-        verify(asylumCase).write(
-            LIST_CASE_HEARING_DATE,
-            LocalDateTime.of(2023, 9, 29, 10, 0)
-                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS"))
-        );
-        verify(coreCaseDataService).triggerEvent(LIST_CASE, CASE_REF, asylumCase);
+        verify(coreCaseDataService).triggerEvent(EDIT_CASE_LISTING, CASE_REF, asylumCase);
     }
 
     @Test
@@ -183,7 +178,7 @@ class EditListCaseHandlerTest {
         when(serviceData.read(ServiceDataFieldDefinition.DURATION, Integer.class))
             .thenReturn(Optional.of(100));
 
-        listCaseHandler.handle(serviceData);
+        editListCaseHandler.handle(serviceData);
 
         verify(asylumCase).write(
             LIST_CASE_HEARING_CENTRE, HearingCentre.REMOTE_HEARING
@@ -191,7 +186,7 @@ class EditListCaseHandlerTest {
         verify(asylumCase).write(
             LIST_CASE_HEARING_LENGTH,"100"
         );
-        verify(coreCaseDataService).triggerEvent(LIST_CASE, CASE_REF, asylumCase);
+        verify(coreCaseDataService).triggerEvent(EDIT_CASE_LISTING, CASE_REF, asylumCase);
     }
 
     @Test
@@ -204,7 +199,7 @@ class EditListCaseHandlerTest {
         when(serviceData.read(ServiceDataFieldDefinition.DURATION, Integer.class))
             .thenReturn(Optional.of(100));
 
-        listCaseHandler.handle(serviceData);
+        editListCaseHandler.handle(serviceData);
 
         verify(asylumCase).write(
             LIST_CASE_HEARING_CENTRE, HearingCentre.REMOTE_HEARING
@@ -212,7 +207,7 @@ class EditListCaseHandlerTest {
         verify(asylumCase).write(
             LIST_CASE_HEARING_LENGTH,"100"
         );
-        verify(coreCaseDataService).triggerEvent(LIST_CASE, CASE_REF, asylumCase);
+        verify(coreCaseDataService).triggerEvent(EDIT_CASE_LISTING, CASE_REF, asylumCase);
     }
 
     private void initializeServiceData() {
