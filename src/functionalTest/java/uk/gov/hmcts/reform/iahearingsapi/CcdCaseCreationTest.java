9603a4227462be1dbd2c4195337c1fa5ae4e2996
robin-ieong <robin.ieong@version1.com>
2023-10-31 15:30:34 +0000

    RIA-7943 - Fix checkstyle

--- src/functionalTest/java/uk/gov/hmcts/reform/iahearingsapi/CcdCaseCreationTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iahearingsapi/CcdCaseCreationTest.java
@@ -14,7 +14,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
