7bea65bb1b3fd657c5f9185b603e1c1cfaa35ccd
Marcellofabbri <marcello.fabbri@hmcts.net>
2023-11-13 12:58:59 +0000

    RIA-7700 Booking statuses in otherReasonableAdjustmentsDetails

--- src/test/java/uk/gov/hmcts/reform/iahearingsapi/domain/mappers/WitnessDetailsMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/iahearingsapi/domain/mappers/WitnessDetailsMapperTest.java
@@ -1,16 +1,24 @@
 package uk.gov.hmcts.reform.iahearingsapi.domain.mappers;
 
+import static java.util.Objects.requireNonNullElse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.AsylumCaseFieldDefinition.WITNESS_DETAILS;
+import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.AsylumCaseFieldDefinition.WITNESS_INTERPRETER_SIGN_LANGUAGE_BOOKING_STATUS_1;
+import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.AsylumCaseFieldDefinition.WITNESS_INTERPRETER_SPOKEN_LANGUAGE_BOOKING_STATUS_1;
 
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.iahearingsapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iahearingsapi.domain.entities.InterpreterBookingStatus;
 import uk.gov.hmcts.reform.iahearingsapi.domain.entities.WitnessDetails;
 import uk.gov.hmcts.reform.iahearingsapi.domain.entities.ccd.field.IdValue;
 import uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.IndividualDetailsModel;
@@ -33,7 +41,7 @@ class WitnessDetailsMapperTest {
 
         final List<IdValue<WitnessDetails>> witnessDetails = List.of(
             new IdValue<>(
-                "id1", new WitnessDetails("partyId", "witnessName", "witnessFamilyName", null))
+                "1", new WitnessDetails("partyId", "witnessName", "witnessFamilyName", null))
         );
         when(asylumCase.read(WITNESS_DETAILS)).thenReturn(Optional.of(witnessDetails));
 
@@ -49,8 +57,138 @@ class WitnessDetailsMapperTest {
             .partyRole("WITN")
             .build();
         List<PartyDetailsModel> expected = List.of(expectedParty);
-        when(languageAndAdjustmentsMapper.processPartyCaseFlags(asylumCase, expectedParty)).thenReturn(expectedParty);
+        when(languageAndAdjustmentsMapper.processPartyCaseFlags(asylumCase, any(PartyDetailsModel.class)))
+            .thenReturn(expectedParty);
+
+        expected.get(0).getIndividualDetails().setOtherReasonableAdjustmentDetails("");
 
         assertEquals(expected, new WitnessDetailsMapper(languageAndAdjustmentsMapper).map(asylumCase, caseDataMapper));
     }
+
+    @ParameterizedTest
+    @EnumSource(value = InterpreterBookingStatus.class, names = {"BOOKED", "REQUESTED", "CANCELLED", "NOT_REQUESTED"})
+    void should_handle_spoken_interpreter_booking_status(InterpreterBookingStatus bookingStatus) {
+        when(asylumCase.read(WITNESS_INTERPRETER_SPOKEN_LANGUAGE_BOOKING_STATUS_1, InterpreterBookingStatus.class))
+            .thenReturn(Optional.of(bookingStatus));
+        when(asylumCase.read(WITNESS_INTERPRETER_SIGN_LANGUAGE_BOOKING_STATUS_1, InterpreterBookingStatus.class))
+            .thenReturn(Optional.empty());
+
+        when(caseDataMapper.getHearingChannel(asylumCase)).thenReturn("hearingChannel");
+
+        final List<IdValue<WitnessDetails>> witnessDetails = List.of(
+            new IdValue<>(
+                "1", new WitnessDetails("partyId", "witnessName", "witnessFamilyName", null))
+        );
+        when(asylumCase.read(WITNESS_DETAILS)).thenReturn(Optional.of(witnessDetails));
+
+        IndividualDetailsModel individualDetails = IndividualDetailsModel.builder()
+            .firstName("witnessName")
+            .lastName("witnessFamilyName")
+            .preferredHearingChannel("hearingChannel")
+            .build();
+        PartyDetailsModel witnessPartyDetailsModel = PartyDetailsModel.builder()
+            .individualDetails(individualDetails)
+            .partyID("partyId")
+            .partyType("IND")
+            .partyRole("WITN")
+            .build();
+        when(languageAndAdjustmentsMapper.processPartyCaseFlags(eq(asylumCase), any(PartyDetailsModel.class)))
+            .thenReturn(witnessPartyDetailsModel);
+
+        String status = bookingStatus != InterpreterBookingStatus.NOT_REQUESTED
+            ? " Status: " + bookingStatus.getDesc() + ";"
+            : "";
+
+        witnessPartyDetailsModel.getIndividualDetails().setOtherReasonableAdjustmentDetails(
+            (requireNonNullElse(witnessPartyDetailsModel.getIndividualDetails().getOtherReasonableAdjustmentDetails(),
+                               "") + status).trim());
+
+        assertEquals(List.of(witnessPartyDetailsModel), new WitnessDetailsMapper(languageAndAdjustmentsMapper)
+            .map(asylumCase, caseDataMapper));
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = InterpreterBookingStatus.class, names = {"BOOKED", "REQUESTED", "CANCELLED", "NOT_REQUESTED"})
+    void should_handle_sign_interpreter_booking_status(InterpreterBookingStatus bookingStatus) {
+        when(asylumCase.read(WITNESS_INTERPRETER_SPOKEN_LANGUAGE_BOOKING_STATUS_1, InterpreterBookingStatus.class))
+            .thenReturn(Optional.empty());
+        when(asylumCase.read(WITNESS_INTERPRETER_SIGN_LANGUAGE_BOOKING_STATUS_1, InterpreterBookingStatus.class))
+            .thenReturn(Optional.of(bookingStatus));
+
+        when(caseDataMapper.getHearingChannel(asylumCase)).thenReturn("hearingChannel");
+
+        final List<IdValue<WitnessDetails>> witnessDetails = List.of(
+            new IdValue<>(
+                "1", new WitnessDetails("partyId", "witnessName", "witnessFamilyName", null))
+        );
+        when(asylumCase.read(WITNESS_DETAILS)).thenReturn(Optional.of(witnessDetails));
+
+        IndividualDetailsModel individualDetails = IndividualDetailsModel.builder()
+            .firstName("witnessName")
+            .lastName("witnessFamilyName")
+            .preferredHearingChannel("hearingChannel")
+            .build();
+        PartyDetailsModel witnessPartyDetailsModel = PartyDetailsModel.builder()
+            .individualDetails(individualDetails)
+            .partyID("partyId")
+            .partyType("IND")
+            .partyRole("WITN")
+            .build();
+        when(languageAndAdjustmentsMapper.processPartyCaseFlags(eq(asylumCase), any(PartyDetailsModel.class)))
+            .thenReturn(witnessPartyDetailsModel);
+
+        String status = bookingStatus != InterpreterBookingStatus.NOT_REQUESTED
+            ? " Status: " + bookingStatus.getDesc() + ";"
+            : "";
+
+        witnessPartyDetailsModel.getIndividualDetails().setOtherReasonableAdjustmentDetails(
+            (requireNonNullElse(witnessPartyDetailsModel.getIndividualDetails().getOtherReasonableAdjustmentDetails(),
+                                "") + status).trim());
+
+        assertEquals(List.of(witnessPartyDetailsModel), new WitnessDetailsMapper(languageAndAdjustmentsMapper)
+            .map(asylumCase, caseDataMapper));
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = InterpreterBookingStatus.class, names = {"BOOKED", "REQUESTED", "CANCELLED", "NOT_REQUESTED"})
+    void should_handle_both_spoken_and_sign_interpreter_booking_status(InterpreterBookingStatus bookingStatus) {
+        when(asylumCase.read(WITNESS_INTERPRETER_SPOKEN_LANGUAGE_BOOKING_STATUS_1, InterpreterBookingStatus.class))
+            .thenReturn(Optional.of(bookingStatus));
+        when(asylumCase.read(WITNESS_INTERPRETER_SIGN_LANGUAGE_BOOKING_STATUS_1, InterpreterBookingStatus.class))
+            .thenReturn(Optional.of(bookingStatus));
+
+        when(caseDataMapper.getHearingChannel(asylumCase)).thenReturn("hearingChannel");
+
+        final List<IdValue<WitnessDetails>> witnessDetails = List.of(
+            new IdValue<>(
+                "1", new WitnessDetails("partyId", "witnessName", "witnessFamilyName", null))
+        );
+        when(asylumCase.read(WITNESS_DETAILS)).thenReturn(Optional.of(witnessDetails));
+
+        IndividualDetailsModel individualDetails = IndividualDetailsModel.builder()
+            .firstName("witnessName")
+            .lastName("witnessFamilyName")
+            .preferredHearingChannel("hearingChannel")
+            .build();
+        PartyDetailsModel witnessPartyDetailsModel = PartyDetailsModel.builder()
+            .individualDetails(individualDetails)
+            .partyID("partyId")
+            .partyType("IND")
+            .partyRole("WITN")
+            .build();
+        when(languageAndAdjustmentsMapper.processPartyCaseFlags(eq(asylumCase), any(PartyDetailsModel.class)))
+            .thenReturn(witnessPartyDetailsModel);
+
+        String status = bookingStatus != InterpreterBookingStatus.NOT_REQUESTED
+            ? " Status (Spoken): " + bookingStatus.getDesc() + "; Status (Sign): " + bookingStatus.getDesc() + ";"
+            : "";
+
+        witnessPartyDetailsModel.getIndividualDetails().setOtherReasonableAdjustmentDetails(
+            (requireNonNullElse(witnessPartyDetailsModel.getIndividualDetails().getOtherReasonableAdjustmentDetails(),
+                                "") + status).trim());
+
+        assertEquals(List.of(witnessPartyDetailsModel), new WitnessDetailsMapper(languageAndAdjustmentsMapper)
+            .map(asylumCase, caseDataMapper));
+    }
+
 }
