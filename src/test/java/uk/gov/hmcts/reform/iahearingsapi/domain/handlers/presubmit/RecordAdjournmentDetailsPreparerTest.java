5b6fc4a003cf74b35ed0aa7000b53cb27b237896
Miguel Martin <miguel.martin1@hmcts.net>
2023-11-22 13:16:49 +0100

    RIA-7768 Fix Adjournment tests

--- src/test/java/uk/gov/hmcts/reform/iahearingsapi/domain/handlers/presubmit/RecordAdjournmentDetailsPreparerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iahearingsapi/domain/handlers/presubmit/RecordAdjournmentDetailsPreparerTest.java
@@ -9,11 +9,10 @@ import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.AsylumCaseFieldDefinition.ADJOURNMENT_DETAILS_HEARING;
 import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.ccd.Event.RECORD_ADJOURNMENT_DETAILS;
 import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.ccd.callback.PreSubmitCallbackStage.ABOUT_TO_START;
+import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.HmcStatus.AWAITING_LISTING;
 import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.HmcStatus.CANCELLATION_SUBMITTED;
-import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.HmcStatus.HEARING_REQUESTED;
 import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.HmcStatus.UPDATE_REQUESTED;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -42,7 +41,6 @@ public class RecordAdjournmentDetailsPreparerTest {
     private static final String HEARING_TYPE_DESCRIPTION_SUBSTANTIVE = "Substantive";
     private static final String HEARING_TYPE_DESCRIPTION_CASE_MANAGEMENT_REVIEW = "Case Management Review";
     private static final String HEARING_TYPE_DESCRIPTION_COSTS = "Costs";
-    private static final LocalDateTime HEARING_REQUEST_DATE_TIME = LocalDateTime.of(2023, 10, 19, 12, 0);
 
     @Mock
     private Callback<AsylumCase> callback;
@@ -85,15 +83,12 @@ public class RecordAdjournmentDetailsPreparerTest {
         when(caseHearing1.getHearingRequestId()).thenReturn(CASE_HEARING_REQUEST_ID_1);
         when(caseHearing2.getHearingRequestId()).thenReturn(CASE_HEARING_REQUEST_ID_2);
         when(caseHearing3.getHearingRequestId()).thenReturn(CASE_HEARING_REQUEST_ID_3);
-        when(caseHearing1.getHmcStatus()).thenReturn(HEARING_REQUESTED);
+        when(caseHearing1.getHmcStatus()).thenReturn(AWAITING_LISTING);
         when(caseHearing2.getHmcStatus()).thenReturn(UPDATE_REQUESTED);
         when(caseHearing3.getHmcStatus()).thenReturn(CANCELLATION_SUBMITTED); // disqualifies hearing for selection
         when(caseHearing1.getHearingTypeDescription()).thenReturn(HEARING_TYPE_DESCRIPTION_SUBSTANTIVE);
         when(caseHearing2.getHearingTypeDescription()).thenReturn(HEARING_TYPE_DESCRIPTION_CASE_MANAGEMENT_REVIEW);
         when(caseHearing3.getHearingTypeDescription()).thenReturn(HEARING_TYPE_DESCRIPTION_COSTS);
-        when(caseHearing1.getHearingRequestDateTime()).thenReturn(HEARING_REQUEST_DATE_TIME);
-        when(caseHearing2.getHearingRequestDateTime()).thenReturn(HEARING_REQUEST_DATE_TIME);
-        when(caseHearing3.getHearingRequestDateTime()).thenReturn(HEARING_REQUEST_DATE_TIME);
         when(hearingsGetResponse.getCaseHearings()).thenReturn(List.of(caseHearing1, caseHearing2, caseHearing3));
 
         PreSubmitCallbackResponse<AsylumCase> callbackResponse = recordAdjournmentDetailsPreparer.handle(
@@ -109,7 +104,7 @@ public class RecordAdjournmentDetailsPreparerTest {
 
         DynamicList result = dynamicListArgumentCaptor.getValue();
         assertEquals(2, result.getListItems().size());
-        assertEquals("Substantive - 19 October 2023", result.getListItems().get(0).getLabel());
-        assertEquals("Case Management Review - 19 October 2023", result.getListItems().get(1).getLabel());
+        assertEquals("Substantive (Waiting to be listed)", result.getListItems().get(0).getLabel());
+        assertEquals("Case Management Review (Update requested)", result.getListItems().get(1).getLabel());
     }
 }
