c4be0236034b9bc6a93a1b3c79c2143da0eee868
Miguel Martin <miguel.martin1@hmcts.net>
2023-12-05 15:55:03 +0100

    RIA-7771 disable functional tests

--- src/functionalTest/java/uk/gov/hmcts/reform/iahearingsapi/RecordAdjournmentUpdateRequestHandlerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iahearingsapi/RecordAdjournmentUpdateRequestHandlerFunctionalTest.java
@@ -19,6 +19,7 @@ import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.test.context.ActiveProfiles;
@@ -30,6 +31,7 @@ import uk.gov.hmcts.reform.iahearingsapi.domain.entities.ccd.CaseDetails;
 import uk.gov.hmcts.reform.iahearingsapi.domain.entities.ccd.callback.Callback;
 
 @Slf4j
+@Disabled
 @ActiveProfiles("functional")
 public class RecordAdjournmentUpdateRequestHandlerFunctionalTest extends CcdCaseCreationTest {
 
