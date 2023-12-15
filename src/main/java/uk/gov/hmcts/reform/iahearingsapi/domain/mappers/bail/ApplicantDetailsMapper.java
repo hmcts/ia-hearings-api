12de420844cedf854eaf4a93a4993f51dfa1247b
Ryan-Nesbitt <95298094+Ryan-Nesbitt@users.noreply.github.com>
2023-12-15 14:29:48 +0000

    RIA-8006 Interpreter booking statuses added to other reasonable adjs (#276)
    Co-authored-by: rajeshthuraiyur <48379561+rajeshthuraiyur@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/iahearingsapi/domain/mappers/bail/ApplicantDetailsMapper.java
+++ src/main/java/uk/gov/hmcts/reform/iahearingsapi/domain/mappers/bail/ApplicantDetailsMapper.java
@@ -1,16 +1,21 @@
 package uk.gov.hmcts.reform.iahearingsapi.domain.mappers.bail;
 
+import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.BailCaseFieldDefinition.APPLICANT_FAMILY_NAME;
+import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.BailCaseFieldDefinition.APPLICANT_GIVEN_NAMES;
+import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.BailCaseFieldDefinition.APPLICANT_INTERPRETER_SIGN_LANGUAGE_BOOKING_STATUS;
+import static uk.gov.hmcts.reform.iahearingsapi.domain.mappers.PartyDetailsMapper.appendBookingStatus;
+
+import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iahearingsapi.domain.entities.BailCase;
 import uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.CustodyStatus;
+import uk.gov.hmcts.reform.iahearingsapi.domain.entities.BailCaseFieldDefinition;
+import uk.gov.hmcts.reform.iahearingsapi.domain.entities.InterpreterBookingStatus;
 import uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.IndividualDetailsModel;
 import uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.PartyDetailsModel;
 import uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.PartyType;
 import uk.gov.hmcts.reform.iahearingsapi.domain.mappers.LanguageAndAdjustmentsMapper;
-
-import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.BailCaseFieldDefinition.APPLICANT_FAMILY_NAME;
-import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.BailCaseFieldDefinition.APPLICANT_GIVEN_NAMES;
 
 @Component
 @AllArgsConstructor
@@ -42,6 +47,21 @@ public class ApplicantDetailsMapper {
 
         languageAndAdjustmentsMapper.processBailPartyCaseFlags(bailCase, applicantPartyDetailsModel);
 
+        appendApplicantBookingStatus(bailCase, applicantPartyDetailsModel);
+
         return applicantPartyDetailsModel;
+    }
+
+    private void appendApplicantBookingStatus(BailCase bailCase,
+                                              PartyDetailsModel applicantPartyDetailsModel) {
+
+        Optional<InterpreterBookingStatus> spokenBookingStatus = bailCase
+            .read(BailCaseFieldDefinition.APPLICANT_INTERPRETER_SPOKEN_LANGUAGE_BOOKING_STATUS,
+                InterpreterBookingStatus.class);
+
+        Optional<InterpreterBookingStatus> signBookingStatus = bailCase
+            .read(APPLICANT_INTERPRETER_SIGN_LANGUAGE_BOOKING_STATUS, InterpreterBookingStatus.class);
+
+        appendBookingStatus(spokenBookingStatus, signBookingStatus, applicantPartyDetailsModel);
     }
 }
