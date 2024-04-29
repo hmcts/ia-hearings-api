9c22d51b5b24b85a74c58a59ace2286db2d2c764
Augustine Onubeze <ikechukwu.onubeze@gmail.com>
2024-04-29 11:55:13 +0100

    RIA-8812: Enable real-time HMC hearing events handling

--- src/test/java/uk/gov/hmcts/reform/iahearingsapi/infrastructure/hmc/listeners/HmcHearingsEventTopicListenerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iahearingsapi/infrastructure/hmc/listeners/HmcHearingsEventTopicListenerTest.java
@@ -7,7 +7,6 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.HmcStatus.EXCEPTION;
-import static uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.HmcStatus.LISTED;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.charset.StandardCharsets;
@@ -70,19 +69,4 @@ class HmcHearingsEventTopicListenerTest {
 
         verify(hmcMessageProcessor, never()).processMessage(any(HmcMessage.class));
     }
-
-    // Disable this test if using live-processing instead of batch-processing of messages
-    @Test
-    public void should_not_process_messages_with_hmc_status_different_than_exception() throws Exception {
-        HmcMessage hmcMessage = TestUtils.createHmcMessage(SERVICE_CODE);
-        hmcMessage.setHearingUpdate(HearingUpdate.builder().hmcStatus(LISTED).build());
-        String stringMessage = OBJECT_MAPPER.writeValueAsString(hmcMessage);
-        byte[] message = StandardCharsets.UTF_8.encode(stringMessage).array();
-
-        given(mockObjectMapper.readValue(any(String.class), eq(HmcMessage.class))).willReturn(hmcMessage);
-        hmcHearingsEventTopicListener.onMessage(message);
-
-        verify(hmcMessageProcessor, never()).processMessage(any(HmcMessage.class));
-    }
-
 }
