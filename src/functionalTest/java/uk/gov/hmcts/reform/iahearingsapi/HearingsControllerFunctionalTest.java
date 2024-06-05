e5887e1c26c2ef54aefc22984f16405c58c571f2
robin-ieong <101253854+robin-ieong@users.noreply.github.com>
2024-06-05 09:31:03 +0100

    RIA-TASK - Test enable fts (#423)
    * Test enable functional tests
    
    * Fix checkstyle

--- src/functionalTest/java/uk/gov/hmcts/reform/iahearingsapi/HearingsControllerFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iahearingsapi/HearingsControllerFunctionalTest.java
@@ -8,7 +8,6 @@ import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -21,7 +20,6 @@ import uk.gov.hmcts.reform.iahearingsapi.domain.entities.HearingRequestPayload;
 @Slf4j
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @ActiveProfiles("functional")
-@Disabled
 class HearingsControllerFunctionalTest extends CcdCaseCreationTest {
     @BeforeEach
     void getAuthentications() {
