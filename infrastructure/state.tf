2186ff7f2326e4fcd627d3871e68add865642df4
Aliveni Choppa <aliveni.choppa1@hmcts.net>
2025-02-28 18:23:02 +0000

    update state.tf

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 4.14.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.0"
+      version = "3.0.2"
     }
   }
 }
