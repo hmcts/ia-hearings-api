87ccc9188d535cb834f59cc8dea5f0895a9fb516
rajeshthuraiyur <48379561+rajeshthuraiyur@users.noreply.github.com>
2025-04-23 11:07:02 +0100

    Update AutoRequestHearingFunctionalTest.java

--- src/functionalTest/java/uk/gov/hmcts/reform/iahearingsapi/AutoRequestHearingFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/iahearingsapi/AutoRequestHearingFunctionalTest.java
@@ -18,7 +18,6 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
