81609904d097396cb6110ea994af59c2e9912dd4
kleog <kleogeorgiou@hotmail.com>
2025-05-08 12:25:04 +0100

    SNI-7482 - update CaseDeepLink for LA on updating hearing request

--- src/main/java/uk/gov/hmcts/reform/iahearingsapi/domain/service/UpdateHearingPayloadService.java
+++ src/main/java/uk/gov/hmcts/reform/iahearingsapi/domain/service/UpdateHearingPayloadService.java
@@ -139,6 +139,7 @@ public class UpdateHearingPayloadService extends CreateHearingPayloadService {
         caseDetails.setCaseAdditionalSecurityFlag(caseFlagsMapper.getCaseAdditionalSecurityFlag(asylumCase));
         caseDetails.setCaseCategories(getCaseCategoriesValue(asylumCase));
         caseDetails.setCaseManagementLocationCode(caseDataMapper.getCaseManagementLocationCode(asylumCase));
+        caseDetails.setCaseDeepLink(caseDataMapper.getCaseDeepLink(caseReference.toString()));
 
         return caseDetails;
     }
