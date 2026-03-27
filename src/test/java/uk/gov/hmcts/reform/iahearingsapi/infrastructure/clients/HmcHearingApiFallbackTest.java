14a3e427cf86bbbc56e72c4a97528fff85f753c3
erclaudio <claudio.andrei@icloud.com>
2026-03-27 13:03:19 +0000

    added fallback methods tests

---
+++ src/test/java/uk/gov/hmcts/reform/iahearingsapi/infrastructure/clients/HmcHearingApiFallbackTest.java
@@ -0,0 +1,102 @@
+package uk.gov.hmcts.reform.iahearingsapi.infrastructure.clients;
+
+import com.github.rjeschke.txtmark.Run;
+import com.launchdarkly.shaded.kotlin.collections.builders.ListBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.HearingsGetResponse;
+import uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.response.CreateHearingRequest;
+import uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.response.PartiesNotified;
+import uk.gov.hmcts.reform.iahearingsapi.domain.entities.hmc.response.UpdateHearingRequest;
+import uk.gov.hmcts.reform.iahearingsapi.infrastructure.clients.model.hmc.DeleteHearingRequest;
+import uk.gov.hmcts.reform.iahearingsapi.infrastructure.clients.model.hmc.HmcHearingResponse;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HmcHearingApiFallbackTest {
+
+    private HmcHearingApiFallback hmcHearingApiFallback;
+    @BeforeEach
+    void setUp() {
+        hmcHearingApiFallback = new HmcHearingApiFallback();
+    }
+
+    @Test
+    void createHearingRequest_throws_runtimeException() {
+        CreateHearingRequest someRequest = new CreateHearingRequest();
+        RuntimeException e = assertThrows(RuntimeException.class, () ->hmcHearingApiFallback.createHearingRequest(
+            "auth", "serviceAuth", "ID",
+            "URL","role", someRequest)
+        );
+        assertEquals("HMC Hearing service: createHearingRequest call failed after retries", e.getMessage());
+    }
+
+    @Test
+    void getHearingRequest_throws_runtimeException() {
+        RuntimeException e = assertThrows(RuntimeException.class, () ->hmcHearingApiFallback.getHearingRequest(
+            "auth","serviceAuth", "ID", "URL", "roleUrl","ID", true
+        ));
+        assertEquals("HMC Hearing service: getHearingRequest call failed after retries", e.getMessage());
+
+
+    }
+
+    @Test
+    void getHearingsRequest_throws_runtimeException() {
+        RuntimeException e = assertThrows(RuntimeException.class, () ->hmcHearingApiFallback.getHearingsRequest(
+            "auth", "serviceAuth","URL","role","depID", "ID"));
+        assertEquals("HMC Hearing service: getHearingsRequest call failed after retries", e.getMessage());
+    }
+
+    @Test
+    void updateHearingRequest_throws_runtimeException() {
+        UpdateHearingRequest someRequest = new UpdateHearingRequest();
+        RuntimeException e = assertThrows(RuntimeException.class, () ->hmcHearingApiFallback.updateHearingRequest(
+            "auth", "serviceAuth","URL","role","depID", someRequest, "id"));
+        assertEquals("HMC Hearing service: updateHearingRequest call failed after retries", e.getMessage());
+    }
+
+    @Test
+    void getPartiesNotifiedRequest_throws_runtimeException() {
+        RuntimeException e = assertThrows(RuntimeException.class, () -> hmcHearingApiFallback.getPartiesNotifiedRequest(
+            "auth", "serviceAuth","URL","role","depID", "id"));
+        assertEquals("HMC Hearing service: getPartiesNotifiedRequest call failed after retries", e.getMessage());
+    }
+
+    @Test
+    void updatePartiesNotifiedRequest_throws_runtimeException() {
+        PartiesNotified parties =  new PartiesNotified();
+        LocalDateTime now = LocalDateTime.now();
+        RuntimeException e = assertThrows(RuntimeException.class, () -> hmcHearingApiFallback.updatePartiesNotifiedRequest(
+            "auth", "serviceAuth","ID","URL","URL", parties, "ID",
+            1, now));
+        assertEquals("HMC Hearing service: updatePartiesNotifiedRequest call failed after retries", e.getMessage());
+    }
+
+    @Test
+    void deleteHearing_throws_503_status() {
+        DeleteHearingRequest someRequest = new DeleteHearingRequest();
+        ResponseEntity<HmcHearingResponse> response = hmcHearingApiFallback.deleteHearing(
+            "auth", "serviceAuth", "ID", "URL",
+            "URL", 9L, someRequest
+        );
+        assertEquals(HttpStatus.SERVICE_UNAVAILABLE, response.getStatusCode());
+    }
+
+    @Test
+    void getUnNotifiedHearings_throws_runtimeException() {
+        LocalDateTime now = LocalDateTime.now();
+        List<String> status = List.of("status", "status2", "status3");
+        RuntimeException e = assertThrows(RuntimeException.class, () ->hmcHearingApiFallback.getUnNotifiedHearings(
+            "auth","service","id","url","url",
+            now,now,status,"id"));
+        assertEquals("HMC Hearing service: getUnNotifiedHearings call failed after retries", e.getMessage());
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/iahearingsapi/infrastructure/clients/IdamApiFallbackTest.java
@@ -0,0 +1,37 @@
+package uk.gov.hmcts.reform.iahearingsapi.infrastructure.clients;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IdamApiFallbackTest {
+
+    private IdamApiFallback idamApiFallback;
+
+    @BeforeEach
+    void setUp() {
+        idamApiFallback = new IdamApiFallback();
+    }
+
+    @Test
+    void userInfo_shouldThrowRuntimeException() {
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                                                  () -> idamApiFallback.userInfo("someToken"));
+
+        assertEquals("IDAM service: userInfo unavailable, call failed after retries",
+                     exception.getMessage());
+    }
+
+    @Test
+    void token_shouldThrowRuntimeException() {
+        Map<String, String> form = Map.of("grant_type", "password");
+
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                                                  () -> idamApiFallback.token(form));
+
+        assertEquals("IDAM service: token unavailable, call failed after retries",
+                     exception.getMessage());
+    }
+}
