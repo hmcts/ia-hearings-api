e1934aacf40b392791be0be95c2876c4eec07ee6
erclaudio <claudio.andrei@icloud.com>
2026-03-27 13:47:13 +0000

    new import for assertions

--- src/test/java/uk/gov/hmcts/reform/iahearingsapi/infrastructure/clients/IdamApiFallbackTest.java
+++ src/test/java/uk/gov/hmcts/reform/iahearingsapi/infrastructure/clients/IdamApiFallbackTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
