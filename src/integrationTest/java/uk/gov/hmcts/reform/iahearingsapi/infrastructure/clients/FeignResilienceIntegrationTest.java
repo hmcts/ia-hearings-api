d01886a5fd69a318b843de4a38ee8b719547e2fc
erclaudio <claudio.andrei@icloud.com>
2026-03-31 14:38:00 +0100

    integration tests

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/iahearingsapi/infrastructure/clients/FeignResilienceIntegrationTest.java
@@ -0,0 +1,141 @@
+package uk.gov.hmcts.reform.iahearingsapi.infrastructure.clients;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("integration")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class FeignResilienceIntegrationTest {
+
+    private WireMockServer wireMockServer;
+
+    @Autowired
+    private IdamApi idamApi;
+
+    @Autowired
+    private HmcHearingApi hmcHearingApi;
+
+    @BeforeEach
+    void setUp() {
+        wireMockServer = new WireMockServer(WireMockConfiguration.options().port(9999));
+        wireMockServer.start();
+        com.github.tomakehurst.wiremock.client.WireMock.configureFor("localhost", 9999);
+    }
+
+    @AfterEach
+    void tearDown() {
+        wireMockServer.stop();
+    }
+
+    @Test
+    void idamApi_shouldTriggerFallback_whenServiceReturns500() {
+        stubFor(get(urlEqualTo("/o/userinfo"))
+                    .willReturn(aResponse()
+                                    .withStatus(500)));
+
+        assertThrows(Exception.class,
+                     () -> idamApi.userInfo("Bearer someToken"));
+    }
+
+    @Test
+    void idamApi_shouldTriggerFallback_whenTokenEndpointReturns500() {
+        stubFor(post(urlEqualTo("/o/token"))
+                    .willReturn(aResponse()
+                                    .withStatus(500)));
+
+        assertThrows(Exception.class,
+                     () -> idamApi.token(java.util.Map.of("grant_type", "password")));
+    }
+
+    @Test
+    void hmcHearingApi_shouldTriggerFallback_whenServiceReturns500() {
+        stubFor(get(urlEqualTo("/hearing/12345"))
+                    .willReturn(aResponse()
+                                    .withStatus(500)));
+
+        assertThrows(Exception.class,
+                     () -> hmcHearingApi.getHearingRequest(
+                         "Bearer token", "serviceAuth",
+                         null, null, null,
+                         "12345", null));
+    }
+
+    @Test
+    void idamApi_shouldTriggerFallback_whenServiceTimesOut() {
+        stubFor(get(urlEqualTo("/o/userinfo"))
+                    .willReturn(aResponse()
+                                    .withFixedDelay(5000)
+                                    .withStatus(200)));
+
+        assertThrows(Exception.class,
+                     () -> idamApi.userInfo("Bearer someToken"));
+    }
+
+    @Test
+    void idamApi_circuitBreaker_shouldOpenAfterRepeatedFailures() {
+        // stub to always return 500
+        stubFor(get(urlEqualTo("/o/userinfo"))
+                    .willReturn(aResponse()
+                                    .withStatus(500)));
+
+        // fire enough requests to trip the circuit breaker
+        // minimumNumberOfCalls is 3 in integration yaml so we need at least 3
+        for (int i = 0; i < 3; i++) {
+            assertThrows(Exception.class,
+                         () -> idamApi.userInfo("Bearer someToken"));
+        }
+
+        // now change wiremock to return 200 - but circuit should still be open
+        stubFor(get(urlEqualTo("/o/userinfo"))
+                    .willReturn(aResponse()
+                                    .withStatus(200)
+                                    .withHeader("Content-Type", "application/json")
+                                    .withBody("{}")));
+
+        // this should still fail because circuit is open
+        assertThrows(Exception.class,
+                     () -> idamApi.userInfo("Bearer someToken"));
+    }
+
+    @Test
+    void hmcHearingApi_circuitBreaker_shouldOpenAfterRepeatedFailures() {
+        stubFor(get(urlEqualTo("/hearing/12345"))
+                    .willReturn(aResponse()
+                                    .withStatus(500)));
+
+        for (int i = 0; i < 3; i++) {
+            assertThrows(Exception.class,
+                         () -> hmcHearingApi.getHearingRequest(
+                             "Bearer token", "serviceAuth",
+                             null, null, null,
+                             "12345", null));
+        }
+
+        stubFor(get(urlEqualTo("/hearing/12345"))
+                    .willReturn(aResponse()
+                                    .withStatus(200)
+                                    .withHeader("Content-Type", "application/json")
+                                    .withBody("{}")));
+
+        assertThrows(Exception.class,
+                     () -> hmcHearingApi.getHearingRequest(
+                         "Bearer token", "serviceAuth",
+                         null, null, null,
+                         "12345", null));
+    }
+}
