d1164231aeb0c4cf1e0a2c1444dc3c2e087e0390
Aliveni Choppa <aliveni.choppa1@hmcts.net>
2026-04-08 15:48:20 +0100

    fix profile values

--- src/main/java/uk/gov/hmcts/reform/iahearingsapi/infrastructure/hmc/UnNotifiedHearingsProcessor.java
+++ src/main/java/uk/gov/hmcts/reform/iahearingsapi/infrastructure/hmc/UnNotifiedHearingsProcessor.java
@@ -35,7 +35,7 @@ import uk.gov.hmcts.reform.iahearingsapi.domain.service.HearingService;
 
 @Slf4j
 @Component
-@Profile("!integration,!functional")
+@Profile("!integration & !functional")
 public class UnNotifiedHearingsProcessor implements Runnable {
 
     private final HmcUpdateDispatcher<ServiceData> dispatcher;
