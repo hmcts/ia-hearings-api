3f880ec52d65089235963055d00585372c286651
Aliveni Choppa <aliveni.choppa1@hmcts.net>
2026-04-13 11:37:44 +0100

    fix service-bus.tf

--- infrastructure/service-bus.tf
+++ infrastructure/service-bus.tf
@@ -1,4 +1,10 @@
 #HMC to hearings API
+
+data "azurerm_servicebus_namespace" "hmc_servicebus_namespace" {
+  name                = join("-", ["hmc-servicebus", var.env])
+  resource_group_name = join("-", ["hmc-shared", var.env])
+}
+
 module "servicebus-subscription" {
   source       = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=4.x"
   name         = "hmc-ia-subs-to-cft-${var.env}"
